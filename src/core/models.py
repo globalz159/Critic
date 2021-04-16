510a2b711afb12f070ce4d9987ad73050224e11b
Gustavo <gustavogontarzik@hotmail.com>
2021-04-16 20:30:58 -0300

    Atualizando a criação de superuser

--- src/core/models.py
+++ src/core/models.py
@@ -27,23 +27,21 @@ class UsuarioManager(BaseUserManager):
     def _create_user(self, username, email, first_name, last_name, password=None, **extra_fields):
         if not email:
             raise ValueError('O campo email é obrigatório')
-        import pdb; pdb.set_trace()
         email = self.normalize_email(email)
-        user = self.model(email=email, username=email, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
     
     def create_user(self, username, email, first_name, last_name, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, email, first_name, last_name, password, **extra_fields)
     
 
     def create_superuser(self, username, email, first_name, last_name, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
-        import pdb; pdb.set_trace()
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, email, first_name, last_name, password, **extra_fields)
 
 
 class Usuario(AbstractUser):
