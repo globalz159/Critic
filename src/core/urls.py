ab3202421e5e62e95baecbf89c94115ec88a1573
Fernando Saeta <fernando.r.saeta@gmail.com>
2021-04-20 00:05:11 -0300

    Atualizando conflito das urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import cadastro, index, v404, v500
+from .views import cadastro, index, v404, v500, carregar_cidades
 
 urlpatterns = [
     path('', index, name='index'),
     path('cadastro', cadastro, name='cadastro'),
     path('404', v404, name='v404'),
-    path('500', v500, name='v500')
+    path('500', v500, name='v500'),
+
+    path('ajax/carregar-cidades/', carregar_cidades, name='ajax_carregar_cidades'),
 ]
 
 
