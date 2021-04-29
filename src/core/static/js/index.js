5ac185eae8971ff0f6765d63a7456152a290c9b5
Fernando Saeta <fernando.r.saeta@gmail.com>
2021-04-28 23:06:50 -0300

    Adicionando script na base.html deixando pagina dos itens com JS herdado

--- src/core/static/js/index.js
+++ src/core/static/js/index.js
@@ -1,14 +1,14 @@
-
-var button = documento.getElementById("action-btn");
+var button = document.getElementById("botao");
 
 button.addEventListener("click",function(){
-    var amigos = document.getElementById(".amigos");
+    var amigos = document.getElementById("amigos");
+    var espaco = document.getElementById("espaco");
 
-    if(amigos.style.display === "none"){
-        amigos.style.display = "block"
-    } else {
+    if(amigos.style.display === "block"){
         amigos.style.display = "none"
+        espaco.style.display = "none"
+    } else {
+        amigos.style.display = "block"
+        espaco.style.display = "block"
     }
-
-
 });
