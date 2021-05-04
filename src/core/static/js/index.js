cff3a3d9b7f2e77915f48019ce3991ac2bc6bf7b
Fernando Saeta <fernando.r.saeta@gmail.com>
2021-05-04 10:54:12 -0300

    Estilizando site e sidebar de amigos

--- src/core/static/js/index.js
+++ src/core/static/js/index.js
@@ -5,9 +5,11 @@ button.addEventListener("click",function(){
     var espaco = document.getElementById("espaco");
 
     if(amigos.style.display === "block"){
+        botao.style.color = "black"
         amigos.style.display = "none"
         espaco.style.display = "none"
     } else {
+        botao.style.color = "white"
         amigos.style.display = "block"
         espaco.style.display = "block"
     }
