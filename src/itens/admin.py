b3ee275fb004ee619443330ce76ada237e2124e9
Fernando Saeta <fernando.r.saeta@gmail.com>
2021-05-10 22:24:07 -0300

    Categorias de itens

--- src/itens/admin.py
+++ src/itens/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Livro, Serie, Filme
+from .models import Livro, Serie, Filme, CategoriaItem
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'slug', 'pais', 'ano_lancamento')
@@ -16,3 +16,7 @@ class LivroAdmin(admin.ModelAdmin):
 @admin.register(Serie)
 class SerieAdmin(admin.ModelAdmin):
     list_display = ItemAdmin.list_display + ('diretor',)
+
+@admin.register(CategoriaItem)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('categoria',)
