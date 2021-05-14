b436a68a599a19ef38738bcabf3598de43e5af73
Fernando Saeta <fernando.r.saeta@gmail.com>
2021-05-13 21:18:27 -0300

    Removendo algumas validações de senha

--- src/Projeto/settings.py
+++ src/Projeto/settings.py
@@ -110,15 +110,6 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
