7c7e3ac6762daf73c8df044f071704f2d85b19f6
Fernando Saeta <fernando.r.saeta@gmail.com>
2021-05-25 10:23:01 -0300

    Atualizando botões dos usuários e status message

--- src/amigos/views.py
+++ src/amigos/views.py
@@ -44,16 +44,27 @@ def adicionar_amigos(request):
 def pedidos_amizade(request):
     context = {}
     app_name = 'amigos'
+    status_message = False
 
     context['app_pedidos'] = True
 
-    pedidos_enviados = request.user.remetente.all()
-    pedidos_recebidos = request.user.destinatario.all()
-    
+    if request.method == 'POST':
+        pedido_obj = PedidosAmizade.objects.get(pk=int(request.POST.get('pedido_id')))
+        if 'aceitar_solicitacao' in request.POST:
+            pedido_obj.aceitar_pedido()
+            status_message = f"Pedido aceito com sucesso! Agora você e {pedido_obj.remetente} são amigos"
+        elif 'excluir_solicitação' in request.POST:
+            pedido_obj.excluir_pedido()
+            status_message = f"Pedido de Amizade Recusado!"
+
+    pedidos_enviados = request.user.remetente.filter(aceito=False)
+    pedidos_recebidos = request.user.destinatario.filter(aceito=False)
+
     context.update({
         'len_resultados': len(pedidos_recebidos),
         'p_enviados': pedidos_enviados,
         'p_recebidos': pedidos_recebidos,
         'app_name': app_name,
+        'status_message': status_message,
     })
     return render(request, 'amigos/pedidos_amizade.html', context)
